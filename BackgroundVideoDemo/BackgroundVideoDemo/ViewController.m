0b8152a2912489bf9e9817747e33d5fd9bee823e
Adam Albarghouthi <adam.albarghouthi@mail.mcgill.ca>
2016-06-26 20:10:08 -0400

    minor

--- BackgroundVideoDemo/BackgroundVideoDemo/ViewController.m
+++ BackgroundVideoDemo/BackgroundVideoDemo/ViewController.m
@@ -20,9 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.backgroundVideo = [[BackgroundVideoObjC alloc] initOnViewController:self withVideoURL:[NSURL URLWithString:@"test.mp4"]];
-    [self.backgroundVideo setUpBackground];
-    
+    self.backgroundVideo = [[BackgroundVideoObjC alloc] initOnViewController:self withVideoURL:[NSURL URLWithString:@"test.mp4"]];    
 }
 
 - (void)didReceiveMemoryWarning {
